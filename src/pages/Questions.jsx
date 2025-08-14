3075806a4f8085e94d7fdeda4ba4e7ffdeb62a6c
Nick.Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2025-08-13 21:12:19 -0400

    Removed a few todos from the list.

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -32,10 +32,7 @@ function Quiz() {
     <main>
       <form onSubmit={checkAnswers}>
         {questions.map((question, index) => {
-          // TODO: Create a question component. Maybe a form with radio buttons?
           // TODO: Figure out a way to mix incorrect and correct answers together randomly
-          // TODO: Figure out if each form has state some how?
-          // TODO: Is Questions component one giant form with a single form action to check all answers?
           // TODO: Track incorrect and correct answers via a tally
           // TODO: Play again function return to Start page.
           // TODO: Add HTML Entities library
